e4b07b718ecd4ac8bf5b13277080434f05c8d6ce
XenosJM <wjdalsqaaz123@gmail.com>
2024-04-30 11:58:31 +0900

    JM : memberVO 및 memberMapper.java/.xml 작성 완료

---
+++ src/main/java/com/soldesk/ex01/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.soldesk.ex01.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class MemberController {
+	// TODO
+}
---
+++ src/main/java/com/soldesk/ex01/persistence/MemberMapper.java
@@ -0,0 +1,19 @@
+package com.soldesk.ex01.persistence;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.soldesk.ex01.domain.MemberVO;
+
+@Mapper
+public interface MemberMapper {
+	int insert(MemberVO memberVO);
+	MemberVO selectByMemberId(int memberId);
+	List<MemberVO> selectIdList();
+	int update(MemberVO memberVO);
+	int updateProperty(MemberVO memberVO);
+	int updateManager(MemberVO memberVO);
+	int delete(int memberId);
+
+}
