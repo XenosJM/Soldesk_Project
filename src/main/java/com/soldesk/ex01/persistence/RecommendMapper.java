66921d0288f9139cd58eb24c71997197140742d9
dmasisissi <gusdn310@naver.com>
2024-07-11 10:48:13 +0900

    추천 수정

--- src/main/java/com/soldesk/ex01/persistence/RecommendMapper.java
+++ src/main/java/com/soldesk/ex01/persistence/RecommendMapper.java
@@ -1,16 +1,17 @@
 package com.soldesk.ex01.persistence;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.soldesk.ex01.domain.RecommendVO;
 
 @Mapper
 public interface RecommendMapper {
 	int insertRecommend(RecommendVO vo);
-	int increaseRecommend(int boardId);
-	int increaseDecommend(int boardId);
+	
+	
 	int deleteRecommend(int boardId);
 	RecommendVO selectRecommend(int boardId);
-	int updateRecommendMember(int boardId, String recommendMember);
+	int updateRecommendMember(RecommendVO recommendVO/*@Param("boardId")int boardId, @Param("recommendMemberString")String recommendMemberString*/);
 	
 }
