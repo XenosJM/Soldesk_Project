a3f75e8e281aaed964258c47c394c31c97fa2441
dmasisissi <gusdn310@naver.com>
2024-07-11 17:54:52 +0900

    Security permitall 추가 및 mapper 수정

--- src/main/java/com/soldesk/ex01/config/SecurityConfig.java
+++ src/main/java/com/soldesk/ex01/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// 권한 설정 및 로그인 로그아웃 관련
 		httpSecurity
 			.authorizeRequests()  // 요청에 권한 부여
-				.antMatchers("/", "/member/regist", "/member/findIdPw", "/login/check", "/board/list", "/board/detail", "/board/search", "/util/**","/reply/{boardId}","/rereply/{boardId}" ).permitAll()  // 루트 URL에 대한 모든 사용자 접근
+				.antMatchers("/", "/member/regist", "/member/findIdPw", "/login/check", "/board/list", "/board/detail", "/board/search", "/util/**","/reply/{boardId}","/rereply/{boardId}","/category/list","/category/detail","/board/recommendlist" ).permitAll()  // 루트 URL에 대한 모든 사용자 접근
 				.antMatchers("/member/**", "/friend/**", "/reply/**", "/rereply/**", "/attach/**", "/board/**").hasAnyRole("MEMBER", "MANAGER", "HEAD_MANAGER")  // 루트 URL에 대한 MEMBER 역할을 가진 사용자만 접근 가능
 				.antMatchers("/ROLE/**").hasRole("HEAD_MANAGER")
 				
